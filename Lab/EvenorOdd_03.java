d73b6c9e7f2e5047c7cff41eb00df53c741a9b30
DeyanFilipov <didkofilipov.sv@gmail.com>
2022-09-17 13:19:52 +0300

    Commit

---
+++ Lab/EvenorOdd_03.java
@@ -0,0 +1,17 @@
+package ConditionalStatements.Lab;
+
+import java.util.Scanner;
+
+public class EvenorOdd_03 {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+         int num = Integer.parseInt(scanner.nextLine());
+         if (num % 2 == 0) {
+             System.out.println("even");
+         } else {
+             System.out.println("odd");
+         }
+
+    }
+}

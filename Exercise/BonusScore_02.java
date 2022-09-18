d080cd4dc2c2c4942b224e9ee49ee14001dcc1cf
DeyanFilipov <didkofilipov.sv@gmail.com>
2022-09-18 12:02:05 +0300

    Commit

--- Exercise/BonusScore_02.java
+++ Exercise/BonusScore_02.java
@@ -1,8 +1,6 @@
 package ConditionalStatements.Exercise;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
-
 public class BonusScore_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
